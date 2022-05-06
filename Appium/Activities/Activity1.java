0f2ece7b013800bd58124a97cd9177897afd1b91
sri5696 <39076984+sri5696@users.noreply.github.com>
2022-05-06 15:54:02 +0530

    Add files via upload

---
+++ Appium/Activities/Activity1.java
@@ -0,0 +1,62 @@
+package Appium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+public class Activity1 {
+	
+	// Declare Android driver
+    AndroidDriver<MobileElement> driver;
+ 
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        // Set the Desired Capabilities
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "Swetha Appium Pie");
+        caps.setCapability("platformName", "Android");
+        caps.setCapability("automationName", "UiAutomator2");
+        caps.setCapability("appPackage", "com.android.calculator2");
+        caps.setCapability("appActivity", ".Calculator");
+        caps.setCapability("noReset", true);
+ 
+        // Instantiate Appium Driver
+        URL appServer = new URL("http://127.0.0.1:4723/wd/hub");
+        driver = new AndroidDriver<MobileElement>(appServer, caps);
+    }
+
+    @Test
+    public void add() {
+        // Using resource-id
+        driver.findElementById("digit_8").click();
+        // Using Accessibility ID
+        driver.findElementByAccessibilityId("multiply").click();
+        // Using XPath
+        driver.findElementByXPath("//android.widget.Button[@text='6']").click();
+ 
+        // Perform Calculation
+        driver.findElementById("eq").click();
+ 
+        // Display Result
+        String result = driver.findElement(MobileBy.id("result")).getText();
+        System.out.println(result);
+ 
+        // Assertion
+        Assert.assertEquals(result, "48");
+    }
+    
+    @AfterClass
+    public void tearDown() {
+        // Close app
+        driver.quit();
+    }
+}
